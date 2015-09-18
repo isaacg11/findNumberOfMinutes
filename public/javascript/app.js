8d1fbdf81c028e6b61af7280bb8ef0e8d82a9076
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-09-17 19:53:17 -0700

    initial commit

---
+++ public/javascript/app.js
@@ -0,0 +1,8 @@
+
+var pm=1200;
+var am=1100;
+
+var CountMinutes=function(){
+  console.log("There is"+" "+(pm-am)*0.6+" "+"minutes between"+" "+(am)+"am"+" "+"and"+" "+(pm)+"pm");
+  };
+CountMinutes();
